1a1d8084ebc18b1be9d31f122ce469f4d0af38d2
cberkley <cole@coleberkley.com>
2026-07-07 15:23:07 -0400

    fixing json property name attribut in c# project.

--- functions/PortfolioFunctions/Models/ProfessionalProject.cs
+++ functions/PortfolioFunctions/Models/ProfessionalProject.cs
@@ -1,30 +1,38 @@
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace PortfolioFunctions.Models
 {
     public class ProfessionalProject
     {
         [JsonProperty("id")]
+        [JsonPropertyName("id")]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
         [JsonProperty("name")]
+        [JsonPropertyName("name")]
         public string? Name { get; set; }
 
         [JsonProperty("description")]
+        [JsonPropertyName("description")]
         public string? Description { get; set; }
 
         [JsonProperty("developerCommentary")]
+        [JsonPropertyName("developerCommentary")]
         public string? DeveloperCommentary { get; set; }
 
         [JsonProperty("marketingSiteUrl")]
+        [JsonPropertyName("marketingSiteUrl")]
         public string? MarketingSiteUrl { get; set; }
 
         [JsonProperty("demoUrl")]
+        [JsonPropertyName("demoUrl")]
         public string? DemoUrl { get; set; }
 
         [JsonProperty("screenshots")]
+        [JsonPropertyName("screenshots")]
         public List<string> Screenshots { get; set; } = new List<string>();
     }
 }
